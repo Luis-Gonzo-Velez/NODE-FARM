b80ac24ec36a89e1504ca84dda033d8932a44f50
Luis-Gonzo-Velez <gadiel2311@outlook.com>
2026-02-26 18:46:47 -0500

    fix bug

--- index.js
+++ index.js
@@ -28,6 +28,10 @@ const url = require("url");
 
 ////////////////////////////////
 //////SERVER
+
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8");
+const dataObj = JSON.parse(data);
+
 const server = http.createServer((req, res) => {
   const pathName = req.url;
 
@@ -36,13 +40,8 @@ const server = http.createServer((req, res) => {
   } else if (pathName === "/product") {
     res.end("This is the PRODUCT");
   } else if (pathName === "/api") {
-    fs.readFile(`${__dirname}/dev-data/data.json`, "utf-8", (err, data) => {
-      const productData = JSON.parse(data);
-      res.writeHead(200, { "Content-type": "application/json" });
-      res.end(data);
-    });
-
-    //
+    res.writeHead(200, { "Content-type": "application/json" });
+    res.end(data);
   } else {
     res.writeHead(404, {
       "Content-type": "text/html",
@@ -51,7 +50,6 @@ const server = http.createServer((req, res) => {
     res.end("<h1>Page not found!</h1>");
   }
 });
-
 server.listen(8000, "127.0.0.1", () => {
   console.log("Listening to requests on port 8000");
 });
